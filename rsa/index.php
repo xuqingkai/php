c524262d43058e834ac427b81e3f3bfb7f2c1151
徐清凯 <xuqingkai@hotmail.com>
2024-07-01 15:14:32 +0800

    Update index.php

--- rsa/index.php
+++ rsa/index.php
@@ -12,34 +12,80 @@
 	<form class="uk-container">
         <fieldset class="uk-fieldset">
             <legend class="uk-legend uk-text-center">RSA</legend>
-            
-            <div class="uk-grid-collapse uk-flex-middle" uk-grid>
-                <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">data</div>
-                <div class="uk-width-expand"><input class="uk-input" type="text"></div>
-            </div>
-            <div class="uk-grid-collapse uk-flex-middle" uk-grid>
-                <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">public</div>
-                <div class="uk-width-expand">
-                    <textarea class="uk-textarea" rows="5"></textarea>
-                 </div>
-            </div>
-            <div class="uk-grid-collapse uk-flex-middle" uk-grid>
-                <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">private</div>
-                <div class="uk-width-expand">
-                    <textarea class="uk-textarea" rows="5"></textarea>
-                 </div>
-            </div>
-            <div class="uk-grid-collapse uk-flex-middle" uk-grid>
-                <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">mode</div>
-                <div class="uk-width-expand">
-                    <label><input class="uk-radio" type="radio" name="mode" checked> MD5</label>
-                    <label><input class="uk-radio" type="radio" name="mode"> SHA1</label>
-                    <label><input class="uk-radio" type="radio" name="mode"> SHA256</label>
+            <ul class="uk-flex-center" uk-tab>
+                <li class="uk-active"><a href="#">加密</a></li>
+                <li><a href="#">解密</a></li>
+                <li><a href="#">签名</a></li>
+                <li><a href="#">验签</a></li>
+            </ul>
+            <div class="uk-switcher uk-margin">
+                <div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">data</div>
+                        <div class="uk-width-expand"><input class="uk-input" type="text"></div>
+                    </div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">public</div>
+                        <div class="uk-width-expand">
+                            <textarea class="uk-textarea" rows="5"></textarea>
+                         </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">data</div>
+                        <div class="uk-width-expand"><input class="uk-input" type="text"></div>
+                    </div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">private</div>
+                        <div class="uk-width-expand">
+                            <textarea class="uk-textarea" rows="5"></textarea>
+                         </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">data</div>
+                        <div class="uk-width-expand"><input class="uk-input" type="text"></div>
+                    </div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">mode</div>
+                        <div class="uk-width-expand">
+                            <label><input class="uk-radio" type="radio" name="mode" checked> MD5</label>
+                            <label><input class="uk-radio" type="radio" name="mode"> SHA1</label>
+                            <label><input class="uk-radio" type="radio" name="mode"> SHA256</label>
+                        </div>
+                    </div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">private</div>
+                        <div class="uk-width-expand">
+                            <textarea class="uk-textarea" rows="5"></textarea>
+                         </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">data</div>
+                        <div class="uk-width-expand"><input class="uk-input" type="text"></div>
+                    </div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">mode</div>
+                        <div class="uk-width-expand">
+                            <label><input class="uk-radio" type="radio" name="mode" checked> MD5</label>
+                            <label><input class="uk-radio" type="radio" name="mode"> SHA1</label>
+                            <label><input class="uk-radio" type="radio" name="mode"> SHA256</label>
+                        </div>
+                    </div>
+                    <div class="uk-grid-collapse uk-flex-middle" uk-grid>
+                        <div class="uk-width-1-4 uk-width-1-6@s uk-padding-small uk-text-right">public</div>
+                        <div class="uk-width-expand">
+                            <textarea class="uk-textarea" rows="5"></textarea>
+                         </div>
+                    </div>
                 </div>
             </div>
             <div class="uk-text-center">
-                 <button class="uk-button uk-button-primary uk-button-large" type="submit">Encrypt</button>
-                 <button class="uk-button uk-button-primary uk-button-large" type="submit">Decrypt</button>
+                 <button class="uk-button uk-button-primary uk-button-large" type="submit">Generate</button>
             </div>
         </fieldset>
     </form>
