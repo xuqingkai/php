711ca06f523794f42b84518eac648d83087cfc9f
徐清凯 <xuqingkai@hotmail.com>
2024-07-15 16:48:15 +0800

    Create agent_pay_config.php

---
+++ pay/agent_pay/agent_pay_config.php
@@ -0,0 +1,7 @@
+<?php
+$agent_pay['host']='http://47.96.103.163:8080';
+$agent_pay['path']='/merchant/payment';
+$agent_pay['merchantId']='11000009725';
+$agent_pay['userAccount']='xschjx';
+$agent_pay['password']='STNqZTVN';
+$agent_pay['merUserId']='';
