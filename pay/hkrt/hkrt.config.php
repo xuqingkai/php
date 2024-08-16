43f61bdc18dbc41d5bc682d3674a6402c379665e
徐清凯 <xuqingkai@hotmail.com>
2024-08-16 17:36:34 +0800

    Update hkrt.config.php

--- pay/hkrt/hkrt.config.php
+++ pay/hkrt/hkrt.config.php
@@ -1,4 +1,5 @@
 <?php
+//http://opendocs.hkrt.cn:8181/docs/saas/saas-1b84emqc7p18m
 $hkrt['host']='https://saas-front.hkrt.cn';
 $hkrt['path']='/api/v2/pay/pre-pay';
 $hkrt['merch_no_list']=explode(',','833466873720000');
