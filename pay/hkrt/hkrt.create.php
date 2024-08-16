7929ee99336329cd62bc13f6c2fba907f6b107cf
徐清凯 <xuqingkai@hotmail.com>
2024-08-16 17:37:10 +0800

    Update hkrt.create.php

--- pay/hkrt/hkrt.create.php
+++ pay/hkrt/hkrt.create.php
@@ -1,16 +1,6 @@
 <?php
+include_once('./hkrt.config.php');
 header('Content-type:application/json');
-$hkrt['host']='https://saas-front.hkrt.cn';
-$hkrt['path']='/api/v2/pay/pre-pay';
-$hkrt['merch_no_list']=explode(',','833466873720000');
-$hkrt['merch_no']=$hkrt['merch_no_list'][rand(1,count($hkrt['merch_no_list']))-1];
-
-$hkrt['agent_no']='';
-$hkrt['accessid']='';
-$hkrt['accesskey']='';
-
-$hkrt['translatekey']='';
-$hkrt['password']='';
 
 $hkrt['data']=[];
 $hkrt['data']['agent_no']=$hkrt['agent_no'];
