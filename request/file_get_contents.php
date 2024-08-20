a74525c889ad1e7eb094f49379ff51f3e2ae922d
徐清凯 <xuqingkai@hotmail.com>
2024-08-20 08:55:55 +0800

    Update file_get_contents.php

--- request/file_get_contents.php
+++ request/file_get_contents.php
@@ -1,4 +1,11 @@
 <?php
 $url='';
 $data='';
-$response=file_get_contents($url, false, stream_context_create(array('http'=>array('method'=>'POST','header'=>"Content-type:application/x-www-form-urlencoded\r\nUser-Agent:AppBrowser",'content'=>$data),'ssl'=>array('verify_peer'=>false, 'verify_peer_name'=>false))));
+$response=file_get_contents($url, false, stream_context_create(array(
+  'http'=>array(
+      'method'=>'POST',
+      'header'=>"Content-type:application/x-www-form-urlencoded\r\nUser-Agent:AppBrowser",
+      'content'=>$data
+  ),
+  'ssl'=>array('verify_peer'=>false,'verify_peer_name'=>false)
+)));
