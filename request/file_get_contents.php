eb4f3ecece592242022752ee3e9c540f5bc9f338
徐清凯 <xuqingkai@hotmail.com>
2024-08-20 08:59:31 +0800

    Update file_get_contents.php

--- request/file_get_contents.php
+++ request/file_get_contents.php
@@ -9,3 +9,8 @@ $response=file_get_contents($url, false, stream_context_create(array(
   ),
   'ssl'=>array('verify_peer'=>false,'verify_peer_name'=>false)
 )));
+foreach($http_response_header as $response_header){
+  if(substr(strtolower($response_header),0,10)=='set-cookie'){
+    
+  }
+}
