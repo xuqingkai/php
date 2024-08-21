5e3b07960e4b98d2f6e6ecfe490a7b82d36addea
徐清凯 <xuqingkai@hotmail.com>
2024-08-21 14:41:56 +0800

    Update hkrt.pn.php

--- pay/hkrt/hkrt.pn.php
+++ pay/hkrt/hkrt.pn.php
@@ -18,7 +18,7 @@ $hkrt['request']['code']='11';
 
 $hkrt['sign_string']='';
 ksort($hkrt['request']);
-foreach($hkrt['request'] as $key=>$val){ $hkrt['sign_string'].='&'.$key.'='.$val; }
+foreach($hkrt['request'] as $key=>$val){ if(strtolower($key)!='sign' && strlen($val)>0){$hkrt['sign_string'].='&'.$key.'='.$val;} }
 $hkrt['sign_string']=substr($hkrt['sign_string'],1).$hkrt['accesskey'];
 $hkrt['request']['sign']=strtoupper(md5($hkrt['sign_string']));
 $hkrt['request_string']=json_encode($hkrt['request'],JSON_UNESCAPED_UNICODE);
