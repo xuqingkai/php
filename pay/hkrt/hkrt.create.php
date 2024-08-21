ff74015d0e9d6ae38f6f151e9f6b5969c9eb9678
徐清凯 <xuqingkai@hotmail.com>
2024-08-21 14:17:11 +0800

    Update hkrt.create.php

--- pay/hkrt/hkrt.create.php
+++ pay/hkrt/hkrt.create.php
@@ -25,7 +25,27 @@ $hkrt['request']['sign']=strtoupper(md5($hkrt['sign_string']));
 $hkrt['request_string']=json_encode($hkrt['request'],JSON_UNESCAPED_UNICODE);
 $hkrt['response_string']=file_get_contents($hkrt['host'].$hkrt['path'], false, stream_context_create(array('http'=>array('method'=>'POST','header'=>"Content-type:application/json;charset=UTF-8",'content'=>$hkrt['request_string']),'ssl'=>array('verify_peer'=>false, 'verify_peer_name'=>false))));
 $hkrt['response']=json_decode($hkrt['response_string'], true);
-
+/*
+{
+    "merch_no": "",
+    "sign": "8AC8F4D05CA231F829FAB56149D9F4E7",
+    "ali_trade_no": null,
+    "agent_no": "",
+    "return_msg": "成功",
+    "wc_pay_data": null,
+    "result_msg": "请求成功",
+    "pay_mode": "NATIVE",
+    "out_trade_no": "2024082113424064047",
+    "code_url": null,
+    "uniqr_qr_code": null,
+    "uniqr_redirect_url": null,
+    "trade_no": "AL2408211342410001909600459",
+    "pay_type": "ALI",
+    "result_code": "10000",
+    "ali_qr_code": "",
+    "return_code": "SUCCESS"
+}
+*/
 exit(json_encode($hkrt,JSON_UNESCAPED_UNICODE));
 
 ?>
