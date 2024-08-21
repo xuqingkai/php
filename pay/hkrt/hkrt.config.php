a536036a034cc5ccc3144950fc70e7fd7795ccb9
徐清凯 <xuqingkai@hotmail.com>
2024-08-21 17:26:13 +0800

    Update hkrt.config.php

--- pay/hkrt/hkrt.config.php
+++ pay/hkrt/hkrt.config.php
@@ -4,7 +4,7 @@
 $hkrt['host']='https://saas-front.hkrt.cn';
 $hkrt['path']='/api/v2/pay/pre-pay';
 
-$hkrt['merch_no_list']=['833466873720000','833466873720001'];
+$hkrt['merch_no']=['833466873720000','833466873720001'];
 $hkrt['agent_no']='';
 $hkrt['accessid']='';
 $hkrt['accesskey']='';
