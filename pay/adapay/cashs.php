eb08164392761b2a655dd8f012f82d725c6d202e
徐清凯 <xuqingkai@hotmail.com>
2024-10-11 16:25:14 +0800

    Update cashs.php

--- pay/adapay/cashs.php
+++ pay/adapay/cashs.php
@@ -19,7 +19,7 @@ $adapay['response']['data']=json_decode($adapay['response']['data'], true);
 file_put_contents('cashs_'.$adapay['response']['data']['member_id'].'_'.$adapay['response']['data']['order_no'].'.txt', json_encode($adapay));
 
 //{"data":{"app_id":"app_****","cash_amt":"1.00","cash_type":"DM","created_time":"1728611488","fee_amt":"0.00","id":"0021110690630794121383936","member_id":"****","object":"cash","order_no":"cashs20241011095128","real_amt":"1.00","status":"pending","prod_mode":"true"},"signature":"PE3+hgae5jekpZ0qw8q6UCFCHPSd6Kdf\/jCXq0nL657\/qwyBMhuUMVDaqKakRfSjQtYH73UGoxPslGnbf070+xWIl2HTB4ALtLl9ybvYGT7K+d4I7PCQVVoqisIgZlJYRTd+3qDfFropYJaaycZPQ1rxkn02XEnrb3pHQu16NkM="}
-if($adapay['response']['data']['status']=='succeeded'){
+if($adapay['response']['data']['status']=='pending'){
     exit('<a href="cashs.stat.php?order_no='.$adapay['response']['data']['order_no'].'">'.$adapay['response']['data']['order_no'].'</a>');
 }else{
     header('Content-Type: application/json; charset=utf-8');
