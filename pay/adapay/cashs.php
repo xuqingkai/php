01a21073aef5492a1eb7797bb925063cfef994a1
徐清凯 <xuqingkai@hotmail.com>
2024-10-11 09:29:50 +0800

    Create cashs.php

---
+++ pay/adapay/cashs.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);//都显示
+include_once('adapay.config.php');
+$adapay['api_path']='/v1/cashs';
+
+$adapay['data']=[];
+$adapay['data']['app_id']=$adapay['api_id'];
+$adapay['data']['order_no']=$_GET['order_no'];
+$adapay['data']['cash_type']='DM';
+$adapay['data']['cash_amt']='1.00';
+$adapay['data']['member_id']=$adapay['member_id'];
+$adapay['request_data']=json_encode($adapay['data']);
+
+$adapay['response_data']=adapay_request();
+$adapay['response']=json_decode($adapay['response_data'], true);
+$adapay['response']['data']=json_decode($adapay['response']['data'], true);
+file_put_contents('cashs_'.$adapay['data']['member_id'].'.txt', json_encode($adapay));
+exit($adapay['response_data']);
