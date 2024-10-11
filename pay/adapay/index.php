65e229462a087a18a128240ef2f83f28fa64ba21
徐清凯 <xuqingkai@hotmail.com>
2024-10-11 10:29:05 +0800

    Update index.php

--- pay/adapay/index.php
+++ pay/adapay/index.php
@@ -1,2 +1,19 @@
-<?php
-include_once('members.php');
+<!DOCTYPE html>
+<html lang="zh-cmn-Hans">
+<head>
+	<meta charset="utf-8">
+	<title>adapay</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit/dist/css/uikit.min.css" />
+	<script src="https://cdn.jsdelivr.net/npm/uikit/dist/js/uikit.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/uikit/dist/js/uikit-icons.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js"></script>
+	<style type="text/css">
+	    .uk-tab>*>a{font-size:1.0rem;}
+	    .delete,.delete:hover{text-decoration: line-through;}
+	</style>
+</head>
+<body>
+    <p><a href="members.php" class="uk-link">创建用户</a></p>
+</body>
+</html>
