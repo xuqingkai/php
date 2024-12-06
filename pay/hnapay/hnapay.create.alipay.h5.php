523cf3504b1f44dd0cf470b63079b4c546b1ed39
徐清凯 <xuqingkai@hotmail.com>
2024-12-06 15:28:09 +0800

    Update hnapay.create.alipay.h5.php

--- pay/hnapay/hnapay.create.alipay.h5.php
+++ pay/hnapay/hnapay.create.alipay.h5.php
@@ -49,7 +49,7 @@ $hnapay['sign_str'].='signType=['.$hnapay['data']['signType'].']';
 $hnapay['sign_str'].='charset=['.$hnapay['data']['charset'].']';
 $hnapay['sign_str'].='msgCiphertext=['.$hnapay['data']['msgCiphertext'].']';
 
-openssl_sign($hnapay['sign_str'], $hnapay['data']['signValue'], "-----BEGIN RSA PRIVATE KEY-----\n".wordwrap($hnapay['alipay_private_key'], 64, "\n", true)."\n-----END RSA PRIVATE KEY-----", version_compare(PHP_VERSION,'5.4.8','>=') ? OPENSSL_ALGO_SHA1 : SHA1);
+openssl_sign($hnapay['sign_str'], $hnapay['data']['signValue'], $hnapay['xinshoukuan_private_key'], 64, "\n", true), version_compare(PHP_VERSION,'5.4.8','>=') ? OPENSSL_ALGO_SHA1 : SHA1);
 $hnapay['data']['signValue']=base64_encode($hnapay['data']['signValue']);
     
 $html='<form action="'.$hnapay['host'].'/multipay/h5.do'.'" method="post" target="_blank">';
