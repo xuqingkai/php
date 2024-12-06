14d26910fe34f2a0d1dd414af99d815fee20f06a
徐清凯 <xuqingkai@hotmail.com>
2024-12-06 16:41:48 +0800

    Update hnapay.config.php

--- pay/hnapay/hnapay.config.php
+++ pay/hnapay/hnapay.config.php
@@ -35,7 +35,7 @@ $hnapay['merchantId']='';
 上述密钥下载后是个jks文件和一个说明文件（内含各种密码），需要按照以下炒作转为pen格式
 - 安装OpenSSL
 - 在jks文件所在目录下，执行以下命令，先转为pfx格式文件
-- 其中000000为存储密码、111111为别名密码，会提示再次输入密码（第三个），如:222222
+- 其中000000为存储密码、111111为别名，会提示再次输入密码（第三个），如:222222
 
 ```
 keytool -v -importkeystore -srckeystore demo.jks -srcstoretype jks -srcstorepass 000000 -destkeystore demo.pfx -deststoretype pkcs12 -deststorepass 000000 -destkeypass 111111
