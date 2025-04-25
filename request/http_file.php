c5f7ab126abf0ac2207832c39acacc0374ae75f1
徐清凯 <4472979+xuqingkai@users.noreply.github.com>
2025-04-25 23:02:49 +0800

    Update http_file.php

--- request/http_file.php
+++ request/http_file.php
@@ -4,7 +4,6 @@
 //error_reporting(E_ALL);//都显示
 //date_default_timezone_set('PRC');
 function http_file($url, $body='', $headers=array()){
-    global $xuqingkai;
     if(!$headers){
         $headers=array(
             'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0',
