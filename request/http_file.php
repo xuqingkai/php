18c9dac70ce121d52f7bdb4465d5405ee8b755d1
徐清凯 <4472979+xuqingkai@users.noreply.github.com>
2025-04-25 23:01:29 +0800

    Update http_file.php

--- request/http_file.php
+++ request/http_file.php
@@ -3,10 +3,15 @@
 //error_reporting(0);//都不显示
 //error_reporting(E_ALL);//都显示
 //date_default_timezone_set('PRC');
-function http_file($url, $body='', $header=array()){
+function http_file($url, $body='', $headers=array()){
     global $xuqingkai;
-    $headers=array_merge($headers, array('Author'=>'xuqingkai'));
-    $request_headers=array(); foreach($headers as $key=>$val){ $request_headers[]=$key.':'.$val; }
+    if(!$headers){
+        $headers=array(
+            'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0',
+            'Referer:'.substr($url,0,strpos($url,'/',10))
+        );
+    }
+    $headers=array_merge($headers, array('Author:xuqingkai'));
 
     $error=false;
     $response_header=array();
@@ -14,7 +19,7 @@ function http_file($url, $body='', $header=array()){
         $response_body=file_get_contents($url, false, stream_context_create(array(
             'http'=>array(
                 'method'=>strlen($body)>0?'POST':'GET',
-                'header'=>implode("\r\n",$request_headers),
+                'header'=>implode("\r\n",$headers),
                 'content'=>$body
             ),
             'ssl'=>array('verify_peer'=>false,'verify_peer_name'=>false)
