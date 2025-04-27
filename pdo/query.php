48094d7df42f576a6a25757a086cfb29857dea02
徐清凯 <4472979+xuqingkai@users.noreply.github.com>
2025-04-27 15:38:41 +0800

    Update query.php

--- pdo/query.php
+++ pdo/query.php
@@ -3,13 +3,34 @@ error_reporting(E_ALL);//都显示
 
 //$pdo=new PDO('mysql:host=127.0.0.1;dbname=xqk_db;charset=utf8mb4','root','123456');
 $pdo=new PDO('sqlite:./sqlite3.db','123456');
-function pdo_query($sql,$param=array()){
+function pdo_query$sql,$param=false,$where=''){
     global $pdo;
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $error=false;
-    $data=false;
+    $pdo->exec("SET NAMES utf8");
+    $error=false; $data=false;
     try{
         if($param){//参数模式
+            if(substr(strtoUpper($sql),0,7)=='SELECT '){
+                $where='';
+                foreach($param as $key=>$val){ $where.=(strlen($where)>0?' AND ':'').$key.'=:'.$key; }
+                if($where){ $sql.=(strpos($sql, ' WHERE ')!==false?' AND ':' WHERE ').$where; }
+            }elseif(substr(strtoUpper($sql),0,11)=='INSERT INTO'){
+                $fields='';$values='';
+                foreach($param as $key=>$val){
+                    $fields.=strlen($fields)>0?','.$key:$key;
+                    $values.=strlen($values)>0?',:'.$key:':'.$key;
+                }
+                if(strpos($sql, ' ) VALUES ( ')===false){ $sql.=' ('.$fields.') VALUES ('.$values.')'; }
+            }elseif(substr(strtoUpper($sql),0,7)=='UPDATE '){
+                $set='';
+                foreach($param as $key=>$val){ $set.=(strlen($set)>0?',':'').$key.'=:'.$key; }
+                if(strpos($sql, ' SET ')===false){ $sql.=' SET '.$set; }
+                $sql.=(strpos($sql, ' WHERE ')!==false?' AND ':' WHERE ').$where;
+            }elseif(substr(strtoUpper($sql),0,7)=='DELETE '){
+                $where='';
+                foreach($param as $key=>$val){ $where.=(strlen($where)>0?' AND ':'').$key.'=:'.$key; }
+                if($where){ $sql.=(strpos($sql, ' WHERE ')!==false?' AND ':' WHERE ').$where; }
+            }
             $prepare=$pdo->prepare($sql);
             $prepare->execute($param);
             if(substr(strtoUpper($sql),0,7)=='SELECT '){//返回查询数据
@@ -22,6 +43,7 @@ function pdo_query($sql,$param=array()){
                 }
             }
         }else{//纯语句模式
+            
             if(substr(strtoUpper($sql),0,7)=='SELECT '){//返回查询数据
                 $query=$pdo->query($sql);
                 $data=$query->fetchAll(PDO::FETCH_ASSOC);
@@ -30,8 +52,9 @@ function pdo_query($sql,$param=array()){
                 if(substr(strtoUpper($sql),0,11)=='INSERT INTO'){//返回最后插入ID
                     $data=$pdo->lastInsertId();
                 }
-            }
+            }   
         }
+        
     }catch(PDOException $ex){
         $error=$ex->getMessage();
     }
