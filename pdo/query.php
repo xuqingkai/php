15b111d063d8fce07b56a52c9f08baa872de76b7
徐清凯 <4472979+xuqingkai@users.noreply.github.com>
2025-12-05 11:17:41 +0800

    Update query.php

--- pdo/query.php
+++ pdo/query.php
@@ -4,35 +4,21 @@ error_reporting(E_ALL);//都显示
 //$pdo=new PDO('mysql:host=127.0.0.1;dbname=xqk_db;charset=utf8mb4','root','123456');
 $pdo=new PDO('sqlite:./sqlite3.db','123456');
 function pdo_query($sql,$param=false){
-    global $pdo;
+    global $xuqingkai;
+    $pdo=new PDO($xuqingkai['pdo']['dsn']);
+    if(isset($xuqingkai['pdo']['charset'])){ $pdo->exec("SET NAMES utf8"); }
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->exec("SET NAMES utf8");
     $error=false; $data=false;
     try{
         if($param){//参数模式
-            if(substr(strtoUpper($sql),0,7)=='SELECT '){
-                if($where){
-                    $sql.=(strpos($sql, ' WHERE ')===false?' WHERE ':' AND ').$where;
-                }
-            }elseif(substr(strtoUpper($sql),0,11)=='INSERT INTO'){
-                $fields='';$values='';
-                foreach($param as $key=>$val){
-                    $fields.=strlen($fields)>0?','.$key:$key;
-                    $values.=strlen($values)>0?',:'.$key:':'.$key;
-                }
+            if(substr(strtoUpper($sql),0,11)=='INSERT INTO'){
                 if(strpos($sql, ' ) VALUES ( ')===false){
+                    $fields='';$values='';
+                    foreach($param as $key=>$val){
+                        $fields.=strlen($fields)>0?','.$key:$key;
+                        $values.=strlen($values)>0?',:'.$key:':'.$key;
+                    }
                     $sql.=' ('.$fields.') VALUES ('.$values.')'; 
-                }
-            }elseif(substr(strtoUpper($sql),0,7)=='UPDATE '){
-                if($where){
-                    $set='';
-                    foreach($param as $key=>$val){ $set.=(strlen($set)>0?',':'').$key.'=:'.$key; }
-                    $sql.=(strpos($sql, ' SET ')===false?' SET ':',').$set;
-                    $sql.=(strpos($sql, ' WHERE ')===false?' WHERE ':' AND ').$where;
-                }
-            }elseif(substr(strtoUpper($sql),0,7)=='DELETE '){
-                if($where){
-                    $sql.=(strpos($sql, ' WHERE ')===false?' WHERE ':' AND ').$where; 
                 }
             }
             $prepare=$pdo->prepare($sql);
@@ -47,7 +33,6 @@ function pdo_query($sql,$param=false){
                 }
             }
         }else{//纯语句模式
-            
             if(substr(strtoUpper($sql),0,7)=='SELECT '){//返回查询数据
                 $query=$pdo->query($sql);
                 $data=$query->fetchAll(PDO::FETCH_ASSOC);
@@ -58,7 +43,6 @@ function pdo_query($sql,$param=false){
                 }
             }   
         }
-        
     }catch(PDOException $ex){
         $error=$ex->getMessage();
     }
